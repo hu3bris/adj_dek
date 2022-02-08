0ccda957ba82f11df92d0843fd796452beca904b
hu3bris <98773023+hu3bris@users.noreply.github.com>
2022-02-08 18:04:33 +0100

    Update index.py

--- index.py
+++ index.py
@@ -1,7 +1,18 @@
 from flask import Flask, Response
 app = Flask(__name__)
 
-@app.route('/', defaults={'path': ''})
-@app.route('/<path:path>')
-def catch_all(path):
-    return Response("<h1>Flask</h1><p>You visited: /%s</p>" % (path), mimetype="text/html")
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    name = None
+    temp = None
+    text_ = ("Jeder freche Junge ist in der Garage. Jede junge Frau läuft ins Geschäft. Junges Ding schläft.")
+
+    if request.method == 'POST' and 'name' in request.form:
+        name = request.form['name']
+        temp = name
+        # doc = nlp(name)
+        # name = check_for_adjectives(doc)
+    return render_template('index.html', name=name, temp=temp)
+
+if __name__ == '__main__':
+    app.run(debug=True)
